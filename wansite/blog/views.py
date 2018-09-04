e45cb21b72a6db83d1fb5914615277df68e411ee
David Fogg <vanwhebin@gmail.com>
2018-09-04 17:30:36 +0800

    update template with ORM data

--- wansite/blog/views.py
+++ wansite/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, "blog/home.html", {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+    return render(request, "blog/home.html", {'posts': posts})
 
 def year_archive(request):
     return HttpResponse("<p>Hello World</p>")
