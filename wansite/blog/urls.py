8b00429b1c4efbcf00b0a49d6c093ce434328841
David Fogg <vanwhebin@gmail.com>
2018-09-04 18:46:06 +0800

    add post_detail template

--- wansite/blog/urls.py
+++ wansite/blog/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     path('<int:year>/', views.year_archive),
     path('<int:year>/<int:month>/', views.month_archive),
     path('<int:year>/<int:month>/<slug:slug>/', views.article_detail),
+    path('post/<pk>', views.post_detail, name="post_detail"),
 ]
--- wansite/blog/views.py
+++ wansite/blog/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-
     return render(request, "blog/home.html", {'posts': posts})
 
 def year_archive(request):
@@ -18,4 +18,12 @@ def month_archive(request):
 
 def article_detail(request):
     return render(request, "blog/article_detail.hmtl", {}) 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # print(post)
+    # exit()
+    # post = Post.objects.get(pk=pk)
+    return render(request, "blog/post_detail.html", {'post': post})
+
     
